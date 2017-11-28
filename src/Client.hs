147ff022a216454db1b7f87d90abae03241501f0
sahabi <sahabi@gmail.com>
2017-11-28 13:52:36 -0600

    nothing changed

--- src/Client.hs
+++ src/Client.hs
@@ -4,10 +4,10 @@ import qualified Network.Wreq         as W
 import qualified Network.Wreq.Session as WS
 -- | Client session data
 
-data Client = Client {
-  clientUrl     :: String,    
-  clientOpts    :: W.Options, 
-  clientSession :: WS.Session 
-  } deriving ( Show )
+data Client = Client { 
+                       clientUrl     :: String
+                     , clientOpts    :: W.Options
+                     , clientSession :: WS.Session
+                     } deriving ( Show )
 
 
